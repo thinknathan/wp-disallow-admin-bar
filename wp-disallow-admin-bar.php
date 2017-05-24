ac19800331cbd65eb538c6ca196b935f7867a525
Nathan Bolton <nathan@nathanbolton.ca>
2017-05-23 21:56:14 -0700

    Initial commit

---
+++ wp-disallow-admin-bar.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Disallow Admin Bar
+Plugin URI:   https://thinknathan.ca/
+Description:  Disallow admin bar.
+Version:      1.0.0
+Author:       Think_Nathan
+Author URI:   https://thinknathan.ca/
+License:      MIT License
+*/
+
+add_action('show_admin_bar', '__return_false');
